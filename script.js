5cc66ec048d346960ac62cc7e92d36fed59c15af
sethdanner-8 <sethdanner0803@gmail.com>
2026-08-18 19:52:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
