bfaa06a828cb36ea5ae1ba08eb612b58a376e2a6
sethdanner-8 <sethdanner0803@gmail.com>
2026-08-26 20:14:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+function refreshLayout() {
+    document.body.style.display = "none";
+    document.body.offsetHeight;
+    document.body.style.display = "";
+}
+window.addEventListener("orientationchange", () => {
+    setTimeout(refreshLayout, 200);
+});
